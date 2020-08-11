9a3431d627c777ed4e6149a1d55dd7e5bedabfb2
hoangdo <dovanhoang.it@gmail.com>
2020-08-11 09:22:41 +0700

    Initial Commit

---
+++ File Manager & Browser/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  File Manager & Browser
+//
+//  Created by HoangDo on 8/11/20.
+//  Copyright © 2020 HoangDo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
